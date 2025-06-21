acd8eca8f9463094f68907b2045000a8b146c4e1
Wei Lu <way138730101@gmail.com>
2025-06-21 19:13:18 -0400

    added end screen

---
+++ Quiz Master/Assets/Scripts/EndScreen.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EndScreen : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI finalScoreText;
+    ScoreKeeper scoreKeeper;
+
+    void Start()
+    {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    }
+
+    public void ShowFinalScore()
+    {
+        finalScoreText.text = "Congratulations!\nYou scored" +
+                                scoreKeeper.CalculateScore() + "%";
+    }
+}
